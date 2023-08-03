95b141c93d99b8512849dee17c2aa402b9f4f55f
Andrew Westling <a@andrewwestling.com>
2023-08-03 19:17:56 -0400

    Add Tailwind to Ladle
    So it will correctly style the components in Ladle

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { GlobalProvider } from "@ladle/react";
 import { PluginContext } from "../src/components/PluginContext";
 import { MOCK_PLUGIN as plugin } from "./mockData";
+import "../tailwind.css";
 
 export const Provider: GlobalProvider = ({ children }) => (
 	<PluginContext.Provider value={{ plugin }}>
