b7689adf4dc1ff07ce3046f295c63d71fbae9ca7
Andrew Westling <a@andrewwestling.com>
2023-08-05 11:49:04 -0400

    Fix empty state on SelectFolder

--- src/components/SelectFolder.tsx
+++ src/components/SelectFolder.tsx
@@ -17,6 +17,11 @@ export const SelectFolder = ({
 			{/* Create "All Folders" option at the top */}
 			<option value={""}>🗂️ All Folders</option>
 
+			{/* If no folders, create a disabled "No Folders" option */}
+			{folderNames.length === 0 ? (
+				<option disabled>No Folders</option>
+			) : null}
+
 			{/* Make an option for each folder */}
 			{folderNames.map((folderName) => (
 				<option key={folderName} value={folderName}>
