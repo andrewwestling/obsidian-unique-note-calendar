b9d800c641f3325ec5b029f68d92f98d3dbc425e
Andrew Westling <a@andrewwestling.com>
2023-08-05 21:58:50 -0400

    Include refs in Agenda stories
    I forgot to update these when I added the scrolling refs
    
    I kinda hate how complicated these stories have gotten but here we are

--- src/components/Agenda.stories.tsx
+++ src/components/Agenda.stories.tsx
@@ -1,21 +1,58 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Agenda } from "./Agenda";
 import { MOCK_NOTES_TO_SHOW } from "../../.ladle/mockData";
 
 export const WithNotes = () => {
+	const todayRef = useRef<HTMLDivElement | null>(null);
+	const scrollingContainerRef = useRef<HTMLDivElement | null>(null);
+
+	const [loaded, setLoaded] = useState(false);
+	useEffect(() => {
+		setLoaded(true);
+
+		if (todayRef.current) {
+			console.log("📆 Today: Scrolling to today...");
+			todayRef.current.scrollIntoView({ behavior: "auto" });
+		} else {
+			console.log("📆 Today: No date matches today in the list");
+		}
+	}, [loaded]);
+
 	return (
-		<Agenda
-			notesToShow={MOCK_NOTES_TO_SHOW}
-			onNoteClick={(note) => alert(`Clicked ${note.name}`)}
-		/>
+		<div ref={scrollingContainerRef}>
+			<Agenda
+				todayRef={todayRef}
+				scrollContainerRef={scrollingContainerRef}
+				notesToShow={MOCK_NOTES_TO_SHOW}
+				onNoteClick={(note) => alert(`Clicked ${note.name}`)}
+			/>
+		</div>
 	);
 };
 
 export const Empty = () => {
+	const todayRef = useRef<HTMLDivElement | null>(null);
+	const scrollingContainerRef = useRef<HTMLDivElement | null>(null);
+
+	const [loaded, setLoaded] = useState(false);
+	useEffect(() => {
+		setLoaded(true);
+
+		if (todayRef.current) {
+			console.log("📆 Today: Scrolling to today...");
+			todayRef.current.scrollIntoView({ behavior: "auto" });
+		} else {
+			console.log("📆 Today: No date matches today in the list");
+		}
+	}, [loaded]);
 	return (
-		<Agenda
-			notesToShow={[]}
-			onNoteClick={() => alert("No notes to click")}
-		/>
+		<div ref={scrollingContainerRef}>
+			<Agenda
+				todayRef={todayRef}
+				scrollContainerRef={scrollingContainerRef}
+				notesToShow={[]}
+				onNoteClick={() => alert("No notes to click")}
+			/>
+		</div>
 	);
 };
