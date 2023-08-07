1da23edbfdf0c3f8ac88728b21e6246fb71f8324
Andrew Westling <a@andrewwestling.com>
2023-08-07 19:34:47 -0400

    Remove extra test commands

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-import { Notice, Plugin } from "obsidian";
-import { getNotesWithDates, getFlatFolders } from "src/parseNotes";
+import { Plugin } from "obsidian";
 import {
 	DEFAULT_SETTINGS,
 	PluginSettings,
@@ -15,45 +14,6 @@ export default class UniqueNoteCalendarPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		this.addCommand({
-			id: "test-getNoteDates",
-			name: "Test: Get Note Dates",
-			callback: async () => {
-				const noteDates = await getNotesWithDates(
-					this.app.vault.getMarkdownFiles(),
-					this.settings.uniquePrefixFormat
-				);
-
-				console.log({ noteDates });
-
-				const howMany = noteDates.length;
-				const moreThanOne = howMany > 1;
-
-				new Notice(
-					`📝✅ Found ${howMany} ${
-						moreThanOne ? "notes with dates" : "note with a date"
-					}. Check the console to see the output.`
-				);
-			},
-		});
-
-		this.addCommand({
-			id: "test-getFlatFolders",
-			name: "Test: Make FlatFolders",
-			callback: async () => {
-				const flatFolders = await getNotesWithDates(
-					this.app.vault.getMarkdownFiles(),
-					this.settings.uniquePrefixFormat
-				).then(getFlatFolders);
-
-				console.log({ flatFolders });
-
-				new Notice(
-					"🗂️✅ Made flatFolders. Check the console to see the output."
-				);
-			},
-		});
 
 		this.addCommand({
 			id: "unique-note-calendar--open-in-right-sidebar",
